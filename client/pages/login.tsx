8576738f470214efb448c43ee7ae07af26967875
Levi <shrieverdev@gmail.com>
2021-09-10 17:34:25 -0400

    styled login page

--- client/pages/login.tsx
+++ client/pages/login.tsx
@@ -1,13 +1,54 @@
-import React from 'react'
-import { Layout } from '../components/Layout'
+import { Box, Flex, Link, Button } from "@chakra-ui/react";
+import { Formik, Form } from "formik";
+import { useRouter } from "next/router";
+import React from "react";
+import { Layout } from "../components/Layout";
+import { Wrapper } from "../components/Wrapper";
+import NextLink from "next/link";
+import { InputField } from "../components/InputField";
 
 const login = () => {
-    return (
-        <Layout>
-            <h1>login</h1>
-            <input type="text" />
-        </Layout>
-    )
-}
+  const router = useRouter();
+  return (
+    <Layout variant='small'>
+      <Formik
+        initialValues={{ username: "", password: "" }}
+        onSubmit={() => {
+            // send post to server to login
 
-export default login
+            // if login successful reroute to home
+
+            // else display error
+        }}
+      >
+        {({ isSubmitting }) => (
+          <Form>
+            <InputField
+              name='username'
+              placeholder='username'
+              label='Username'
+            />
+            <Box mt={4}>
+              <InputField
+                name='password'
+                placeholder='password'
+                label='Password'
+                type='password'
+              />
+            </Box>
+            <Button
+              mt={4}
+              type='submit'
+              isLoading={isSubmitting}
+              variantColor='teal'
+            >
+              Login
+            </Button>
+          </Form>
+        )}
+      </Formik>
+    </Layout>
+  );
+};
+
+export default login;
