369f98bc43e278e0bada013312b089eac0fae10c
Levi <shrieverdev@gmail.com>
2021-09-11 20:34:02 -0400

    removed redis

--- server/src/app.ts
+++ server/src/app.ts
@@ -1,45 +1,24 @@
 import express from "express";
 import cors from "cors";
 import http from "http";
-import connectRedis from "connect-redis";
-import session from "express-session";
-import Redis from "ioredis";
+// import connectRedis from "connect-redis";
+// import session from "express-session";
+// import Redis from "ioredis";
 import { Server } from "socket.io";
 import { pool } from "./db";
 import { IMessage } from "./types";
 import { login, register } from "./routes";
-import { COOKIE_NAME, __prod__ } from "./constants";
+import { __prod__ } from "./constants";
 
 const app = express();
-const RedisStore = connectRedis(session);
-const redis = new Redis(process.env.REDIS_URL);
 
-app.set("trust proxy", 1);
 app.use(
   cors({
     origin: process.env.CORS_ORIGIN,
     credentials: true,
   })
 );
-app.use(
-  session({
-    name: COOKIE_NAME,
-    store: new RedisStore({
-      client: redis,
-      disableTouch: true,
-    }),
-    cookie: {
-      maxAge: 1000 * 60 * 60 * 24 * 365 * 10, // 10 years
-      httpOnly: true,
-      sameSite: "lax",
-      secure: __prod__,
-      domain: __prod__ ? "" : undefined,
-    },
-    saveUninitialized: false,
-    secret: process.env.SESSION_SECRET as string,
-    resave: false,
-  })
-);
+
 app.use(express.json());
 
 app.post("/register", register);
@@ -81,7 +60,7 @@ io.on("connection", async socket => {
   });
 });
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT ? parseInt(process.env.PORT) : 5000;
 server.listen(PORT, () => {
   console.log("listening on port " + PORT);
 });
