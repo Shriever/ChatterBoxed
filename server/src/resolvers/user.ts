79b98fe1517bf08736e242a7da42e4751d39a9ac
Levi <shrieverdev@gmail.com>
2021-09-14 14:42:29 -0400

    login and register validation

--- server/src/resolvers/user.ts
+++ server/src/resolvers/user.ts
@@ -1,16 +1,99 @@
-import { Arg, Mutation, Resolver } from "type-graphql";
+import {
+  Arg,
+  Field,
+  InputType,
+  Mutation,
+  ObjectType,
+  Resolver,
+} from "type-graphql";
 import { User } from "../entities/User";
-import { hash } from "argon2";
+import { hash, verify } from "argon2";
+
+@ObjectType()
+class FieldError {
+  @Field()
+  field: string;
+
+  @Field()
+  message: string;
+}
+
+@InputType()
+class UsernamePasswordInput {
+  @Field()
+  username: string;
+
+  @Field()
+  password: string;
+}
+
+@ObjectType()
+class UserResponse {
+  @Field(() => [FieldError], { nullable: true })
+  errors?: FieldError[];
+
+  @Field(() => User, { nullable: true })
+  user?: User;
+}
 
 @Resolver()
 export class UserResolver {
-  @Mutation(() => String)
+  @Mutation(() => UserResponse)
   async register(
-    @Arg("username") username: string,
-    @Arg("password") password: string
-  ) {
+    @Arg("options") options: UsernamePasswordInput
+  ): Promise<UserResponse> {
+    const { username, password } = options;
+    if (username.length < 3) {
+      return {
+        errors: [
+          {
+            field: "Username",
+            message: "Username must be at least 3 characters long.",
+          },
+        ],
+      };
+    }
+    if (password.length < 3) {
+      return {
+        errors: [
+          {
+            field: "Password",
+            message: "Password must be at least 3 characters long.",
+          },
+        ],
+      };
+    }
     const hashedPassword = await hash(password);
-    await User.create({ username, password: hashedPassword }).save();
-    return "bye";
+    const user = await User.create({
+      username,
+      password: hashedPassword,
+    }).save();
+    return { user };
+  }
+
+  @Mutation(() => UserResponse)
+  async login(
+    @Arg("options") options: UsernamePasswordInput
+  ): Promise<UserResponse> {
+    const { username, password } = options;
+
+    const user = await User.findOne({
+      username,
+    });
+
+    if (!user) {
+      return {
+        errors: [{ field: "login", message: "Could not login" }],
+      };
+    }
+    const isValid = verify(user.password, password);
+
+    if (!isValid) {
+      return {
+        errors: [{ field: "login", message: "Wrong password" }],
+      };
+    }
+
+    return { user };
   }
 }
